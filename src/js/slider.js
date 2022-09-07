82ec1f067c2b5a04fd49901e6a43b87b266e929b
Aleksmer <aleksmerz@yandex.ru>
2022-09-07 11:13:22 +0500

    first slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,30 @@
 import Swiper from 'swiper';
 
-export default new Swiper('.swiper-container', {
-  spaceBetween: 20,
-  slidesPerView: 3,
+document.addEventListener("DOMContentLoaded", ()=> {
+  new Swiper('.swiper-container', {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    // simulateTouch: true,
+    // touchRatio: 1,
+    // touchAngle: 45,
+    // grabCursor: true,
+    // slideToClickedSlide: true,
+    // keyboard: {
+    //   enabled: true,
+    //   onlyInVieport: true,
+    //   pageUpDown: true
+    // },
+    // Навигация, текущее положение прогрессбар
+    pagination: {
+      el: '.swiper-pagination',
+      // буллеты
+      clickable: true,
+      // dynamicBullets: true,
+    },
+    // стрелки
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    }
+  })
 });
